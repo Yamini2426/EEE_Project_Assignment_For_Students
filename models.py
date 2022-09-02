e98c37b7f6a829ec2b217f68f5a8af41da762b84
Yamini2426 <105730887+Yamini2426@users.noreply.github.com>
2022-09-02 18:00:01 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,20 +1,16 @@
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -1 : Install package flask_sqlalchemy
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -2 : Create db object from SQLAlchemy class
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+db= SQLAlchemy()
 
 class Employee(db.Model):
     __tablename__ = "Employee"
 
     id = db.Column(db.Integer, primary_key=True)
-    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    # ☛ Req -3 : Define column employee_id with unique key constraint, this should be of type integer
-    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+    employee_id=db.Column(db.Integer(),unique=True)
+
     name = db.Column(db.String())
     age = db.Column(db.Integer())
     position = db.Column(db.String())
@@ -26,4 +22,4 @@ class Employee(db.Model):
         self.position = position
 
     def __repr__(self):
-        return f"{self.name}:{self.employee_id}"
+     return f"{self.name}:{self.employee_id}"
